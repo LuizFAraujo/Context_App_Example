c01a8939ab2e84ee4edb554a7e8365c27d73a959
LuizFAraujo <luizfaraujo.eng@gmail.com>
2023-06-02 00:01:59 -0300

    Criado função, com delay, para simular um tempo de request.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,17 @@
+import { UserAvatar } from './components/Avatar/UserAvatar'
 import Home from './pages/Home'
 
 export default function App() {
+
+  // Função, com delay, para simular um request
+  async function getData() {
+    await new Promise((resolve) => setTimeout(resolve, 3000))
+    return {
+      img: <UserAvatar />,
+      name: "User Name Test"
+    }
+  }
+
   return (
     <>
       <Home />
